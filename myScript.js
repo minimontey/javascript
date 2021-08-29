4300a1a44f795e38ec31687b9210ad58b9fb4054
minimontey <70126104+minimontey@users.noreply.github.com>
2021-08-29 17:38:05 -0500

    update

--- myScript.js
+++ myScript.js
@@ -3,5 +3,5 @@ function display()
     var x = 5;
     var y = 6;
     var z = x + y;
-    alert("Hello! The js file is linked to this alert! Also did you know" + x "plus" + y "equals" z "?");
+    alert("Hello! The js file is linked to this alert! Also did you know " + x  + " plus " + y + " equals " + z + "?");
 }
